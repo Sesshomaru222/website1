b6165761878d9e53dd7a697dded6a0585e0878ae
= <=>
2025-12-18 12:04:52 -0600

    Added pics and popup

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 let els = document.getElementsByClassName("pick");
+let popup = document.getElementsByClassName("popup")[0]
+
+setTimeout(hidePopup, 2000)
 
 console.log(els);
 
-
+function hidePopup()
+{
+    let popup = document.getElementsByClassName("popup")[0]
+    popup.classList.toggle('hidden')
+}
 
 // setInterval(getMessage,3000)
