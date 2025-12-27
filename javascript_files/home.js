c9a510cb07b578f1e8f6648fb2e70bdb2dfdf4cc
= <133679723+Sesshomaru222@users.noreply.github.com>
2025-12-27 13:25:52 -0600

    Homescreen styling and javascript have been adjusted to support mobile screen sizes.

--- javascript_files/home.js
+++ javascript_files/home.js
@@ -5,14 +5,44 @@
 
 
 document.addEventListener("DOMContentLoaded", () => {
-document.addEventListener("scroll", () => {
-
-    // console.log(window.scrollY);
-
-
-    if (window.scrollY > 420 && !isActive)
+    const width = window.innerWidth;
+    console.log(width);
+    document.addEventListener("scroll", () => 
     {
 
+        // console.log(window.scrollY);
+
+        
+        if (width > 1179 && window.scrollY > 420 && !isActive)
+        {
+
+            isActive = true;
+
+            document.querySelector("#home-content div:nth-child(2) h1:first-child").classList.add("appear-delay-1");
+            document.querySelector("#home-content div:nth-child(2) h1:nth-child(2)").classList.add("appear-delay-2");
+            document.querySelector("#home-content div:nth-child(2) p").classList.add("appear-delay-3");
+            document.querySelectorAll("#home-content div:nth-child(3) p").forEach(p => p.classList.add("appear-delay-4"));
+            document.querySelector("#home-content div:nth-child(3) h1").classList.add("appear-delay-4");
+            document.querySelector("#home-content div:nth-child(4)").classList.add("appear-delay-4");
+
+            setInterval(() => 
+            {
+                if(changingText.length > 0)
+                {
+                    textElement.textContent = changingText[index];
+                    index = (index + 1) % changingText.length;
+                }
+
+            }, 3000);
+        
+
+        
+        }
+
+    });
+
+    if (width <= 1179 && !isActive)
+    {
         isActive = true;
 
         document.querySelector("#home-content div:nth-child(2) h1:first-child").classList.add("appear-delay-1");
@@ -22,23 +52,16 @@ document.addEventListener("scroll", () => {
         document.querySelector("#home-content div:nth-child(3) h1").classList.add("appear-delay-4");
         document.querySelector("#home-content div:nth-child(4)").classList.add("appear-delay-4");
 
-        setInterval(() => {
+        setInterval(() => 
+        {
             if(changingText.length > 0)
             {
                 textElement.textContent = changingText[index];
                 index = (index + 1) % changingText.length;
             }
 
-    }, 3000);
-            
-        
+        }, 3000);
         
     }
-
-    
-    
-    
-
-});
 
 });
