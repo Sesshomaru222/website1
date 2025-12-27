34c66469fea9a0e7e24c835e9f918335af377ba7
= <133679723+Sesshomaru222@users.noreply.github.com>
2025-12-27 00:15:34 -0600

    Big Updates to the Home page with the addition of a new javascript file. Hottest anime 2025 embedded with video.

---
+++ javascript_files/home.js
@@ -0,0 +1,44 @@
+    let changingText = ["New Releases", "Top Reviews", "Exclusive Content", "Community Picks"];
+    let index = 0;
+    let textElement = document.querySelector("#changing-text h1");
+    let isActive = false;
+
+
+document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("scroll", () => {
+
+    // console.log(window.scrollY);
+
+
+    if (window.scrollY > 420 && !isActive)
+    {
+
+        isActive = true;
+
+        document.querySelector("#home-content div:nth-child(2) h1:first-child").classList.add("appear-delay-1");
+        document.querySelector("#home-content div:nth-child(2) h1:nth-child(2)").classList.add("appear-delay-2");
+        document.querySelector("#home-content div:nth-child(2) p").classList.add("appear-delay-3");
+        document.querySelectorAll("#home-content div:nth-child(3) p").forEach(p => p.classList.add("appear-delay-4"));
+        document.querySelector("#home-content div:nth-child(3) h1").classList.add("appear-delay-4");
+        document.querySelector("#home-content div:nth-child(4)").classList.add("appear-delay-4");
+
+        setInterval(() => {
+            if(changingText.length > 0)
+            {
+                textElement.textContent = changingText[index];
+                index = (index + 1) % changingText.length;
+            }
+
+    }, 3000);
+            
+        
+        
+    }
+
+    
+    
+    
+
+});
+
+});
