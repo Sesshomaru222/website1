d9be1bdc3132e5050729d58b06250748c970197e
Sesshomaru222 <133679723+Sesshomaru222@users.noreply.github.com>
2025-12-27 18:54:29 -0600

    Fixed the header styling to flow better—although the element should still be turned into a 'header' tag.

--- javascript_files/index.js
+++ javascript_files/index.js
@@ -1,36 +1,20 @@
 
 
-// setTimeout(hidePopup, 2000)
+document.addEventListener('DOMContentLoaded', () => {
 
+    let top = document.getElementsByClassName("top")[0];
+    top.classList.add('hidden');
 
-
-function hidePopup()
-{
-    let popup = document.getElementsByClassName("popup")[0]
-    popup.classList.toggle('hidden')
-}
-
-document.addEventListener('DOMContentLoaded', () => 
-{
-    let els = document.getElementsByClassName("pick");
-    
-    let popup = document.getElementsByClassName("popup")[0]
-
-    let top = document.getElementsByClassName("top")[0]
-    top.classList.add('hidden')
-
-    window.addEventListener('scroll', ()=>{
-        if(window.scrollY > 1800)
-        {
-            top.classList.remove('hidden')
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 1800) {
+            top.classList.remove('hidden');
         }
-        else if(window.scrollY <= 1800)
-        {
-            top.classList.add('hidden')
+        else if (window.scrollY <= 1800) {
+            top.classList.add('hidden');
         }
 
-    })
+    });
 
     top.addEventListener('click', () => window.scrollTo({ top: 0, behavior: 'smooth' }));
-    
-})
+
+});
